98865125e0be889affc9bc3c86ebcbccfddd28a3
visureddy28 <@buruguvismayareddy>
2023-09-26 23:09:00 +0530

    mongodb commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('./app_api/model/db');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var routesApi = require('./app_api/routes/index');
 
 var app = express();
 
@@ -20,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api',routesApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_server/controller/contact.js
+++ app_server/controller/contact.js
@@ -6,5 +6,12 @@ module.exports.aboutus = function(req, res){
     res.render('contact', { title: 'contact' });
   };
   module.exports.reservation = function(req, res){
-    res.render('reservation', { title: 'reservation' });
+    res.render('reservation', { title: 'reservation' ,reservationData:[{
+      name: 'Jane Smith',
+      email: 'janesmith@example.com',
+      phone: '987-654-3210',
+      date: new Date('2023-09-05'),
+      time: '18:30',
+      guests: 2,
+  }]});
   };
